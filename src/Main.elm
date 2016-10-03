193ff0a176ef98ad6cdb6b1ac92daf9bda8f560c
Josh Adams <josh.rubyist@gmail.com>
2016-10-03 17:32:08 -0500

    Handle tracking markers in view
    This doesn't yet successfully remove them from Leaflet though!

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
-import Html exposing (text, Html, button, div, h3)
+import Html exposing (text, Html, button, div, h3, ul, li, a, span)
+import Html.Attributes exposing (href)
 import Html.Events exposing (onClick)
 import Html.App exposing (program)
 import Leaflet.Types exposing (LatLng, ZoomPanOptions, defaultZoomPanOptions, MarkerOptions, defaultMarkerOptions)
@@ -19,6 +20,7 @@ type Msg
     = SetLatLng LatLng
     | GetCenter LatLng
     | AddMarker ( Int, LatLng, String )
+    | RemoveMarker Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -43,10 +45,24 @@ update msg model =
                 , Leaflet.Ports.setMarkers <| markersAsOutboundType newModel.markers
                 )
 
+        RemoveMarker id ->
+            let
+                newModel =
+                    removeMarker id model
+            in
+                ( newModel
+                , Leaflet.Ports.setMarkers <| markersAsOutboundType newModel.markers
+                )
+
 
 addMarker : ( Int, LatLng, String ) -> Model -> Model
 addMarker ( id, markerOptions, popupText ) model =
     { model | markers = Dict.insert id ( markerOptions, popupText ) model.markers }
+
+
+removeMarker : Int -> Model -> Model
+removeMarker id model =
+    { model | markers = Dict.remove id model.markers }
 
 
 markersAsOutboundType : Dict Int ( LatLng, String ) -> List ( Int, LatLng, MarkerOptions, String )
@@ -93,4 +109,22 @@ view model =
         , button [ onClick <| AddMarker ( 1, birminghamLatLng, "Birmingham, AL" ) ] [ text "Add Marker for Birmingham" ]
         , button [ onClick <| AddMarker ( 2, boulderLatLng, "Boulder, CO" ) ] [ text "Add Marker for Boulder" ]
         , h3 [] [ text <| toString model.latLng ]
+        , markersView model
+        ]
+
+
+markersView : Model -> Html Msg
+markersView model =
+    div []
+        [ h3 [] [ text "Markers" ]
+        , ul []
+            (List.map markerView (Dict.toList model.markers))
+        ]
+
+
+markerView : ( Int, ( LatLng, String ) ) -> Html Msg
+markerView ( key, ( latLng, label ) ) =
+    li []
+        [ span [] [ text (label ++ " " ++ (toString latLng)) ]
+        , a [ href "#", onClick <| RemoveMarker key ] [ text "[x]" ]
         ]
